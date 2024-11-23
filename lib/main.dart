136486b56f6644f09c0a70aa494d18c2cea54954
Thiago Luiz de Matos <127130121+thidematos@users.noreply.github.com>
2024-11-23 01:03:20 -0300

    Learning to create custom Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Hello World!',
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.cyan, Colors.blue],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'ASIMOV JR.',
+              style: TextStyle(
+                color: Color.fromARGB(255, 233, 231, 231),
+                fontSize: 42,
               ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
+            ),
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
